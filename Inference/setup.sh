224db8cee2386aaec9a9b64919ec626e9a912686
Giacomo Brunetta <102242995+giacomo-brunetta@users.noreply.github.com>
2024-10-30 23:31:31 -0500

    Refactor folders

---
+++ Inference/setup.sh
@@ -0,0 +1,2 @@
+# pull ML perf
+git clone https://github.com/mlcommons/inference.git
--- getRepoInference.sh
+++
@@ -1,9 +0,0 @@
-#get clone of repo from github
-# Usage: getRepo.sh <repo> <dir> <type>
-# <repo> : github repository
-# <dir> : directory to clone into
-# <type> :  inference or training
-# Example: getRepo.sh
-git clone https://github.com/mlcommons/inference.git
-
-#install dependencies
