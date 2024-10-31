2ce90892d59f23e439461aa094b98bd91b513feb
Giacomo Brunetta <102242995+giacomo-brunetta@users.noreply.github.com>
2024-10-31 00:50:21 -0500

    imagenet download added to setup

--- Inference/setup.sh
+++ Inference/setup.sh
@@ -1,2 +1,13 @@
 # pull ML perf
 git clone https://github.com/mlcommons/inference.git
+
+#get imagenet dataset
+mkdir data
+cd data/
+wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar
+mkdir val
+mv ILSVRC2012_img_val.tar val
+cd val
+tar -xvf ILSVRC2012_img_val.tar 
+wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash
+rm ILSVRC2012_img_val.tar 
